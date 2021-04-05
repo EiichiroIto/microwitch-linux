d5e1631bf0277a7db7e5882bc3ce213aa6048bfe
Eiichiro Ito <eiichiro.ito@gmail.com>
2021-04-05 16:28:36 +0900

    reorganize repository.

--- src/Plugins/amd64/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -f *.o MicrobitPlugin
-gcc -fPIC -Wall -m32 -pedantic-errors -I ../src -c ../src/MicrowitchPlugin.c ../src/unixMicrowitchOps.c
-gcc -m32 -shared *.o -o so.MicrowitchPlugin
-rm -f *.o
--- src/Plugins/armhf/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -f *.o MicrobitPlugin
-gcc -fPIC -Wall -pedantic-errors -I../src -c ../src/MicrowitchPlugin.c ../src/unixMicrowitchOps.c
-gcc -shared *.o -o so.MicrowitchPlugin
-rm -f *.o
